c6588507ada5403da1519aa65499062ac6c8d57f
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-04 17:22:02 +0100

    add posts

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ app.listen(port, () => {
 });
 
 
-
 app.get("/", (req, res) => {
   res.send("Express Blog Routing");
+});
+
+const posts = require('./db/posts.js')
+
+app.get("/posts", (req, res) => {
+  res.json({
+    data: posts,
+    counter: posts.length,
+  });
 });
---
+++ db/posts.js
@@ -0,0 +1,34 @@
+const posts = [
+  {
+    titolo: "Blog Express 1",
+    contenuto: "GATTI",
+    img: "/img/images.jpg",
+    tags: ["#tag1", " #tag2", " #tag3"],
+  },
+  {
+    titolo: "Blog Express 2",
+    contenuto: "GATTI",
+    img: "/img/images.jpg",
+    tags: ["#tag4", " #tag5", " #tag6"],
+  },
+  {
+    titolo: "Blog Express 3",
+    contenuto: "GATTI",
+    img: "/img/images.jpg",
+    tags: ["#tag7", " #tag8", " #tag9"],
+  },
+  {
+    titolo: "Blog Express 4",
+    contenuto: "GATTI",
+    img: "/img/images.jpg",
+    tags: ["#tag10", " #tag11", " #tag12"],
+  },
+  {
+    titolo: "Blog Express 5",
+    contenuto: "GATTI",
+    img: "/img/images.jpg",
+    tags: ["#tag13", " #tag14", " #tag15"],
+  },
+];
+
+module.exports = posts;

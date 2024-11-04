ba31da30d9b6c3fc2598178e2dd19b39e041fd39
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-04 16:30:36 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const host = "http://127.0.0.1";
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on ${host}:${port}`);
+});
+
+
+
+app.get("/", (req, res) => {
+  res.send("Express Blog Routing");
+});
